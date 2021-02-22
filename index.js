8f6b1cca33b9a507914f85107e980483a320c123
habeshawit <shalomseleshi@gmail.com>
2021-02-21 20:27:42 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+var customerName = 'bob';
+var bestCustomer;
+const leastFavoriteCustomer = "jill";
+
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "bob";
+}
+
+// changeLeastFavoriteCustomer() does not reassign leastfavoritecustomer;
+// Uncaught SyntaxError: Identifier 'leastFavoriteCustomer' has already been declared
